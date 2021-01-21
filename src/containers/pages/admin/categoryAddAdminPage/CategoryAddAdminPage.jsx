e08d53131d56e6c6ce5efb4e45720f64502eda1c
xuanthuy78 <phamthixuanthuy78@gmail.com>
2021-01-21 15:26:55 +0700

    fix bug message when add category

--- src/containers/pages/admin/categoryAddAdminPage/CategoryAddAdminPage.jsx
+++ src/containers/pages/admin/categoryAddAdminPage/CategoryAddAdminPage.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import { Form, Input, Button } from 'antd'
+import React, { useState } from 'react'
+import { Form, Input, Button, message } from 'antd'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as Actions from '../../../../actions/index'
 
 function CategoryAddAdminPage(props) {
+  const [required, setRequired] = useState(false)
+  const [notice, setNotice] = useState(null)
+
   const onFinish = (values) => {
-    props.actions.categoryCreate(values)
-    props.history.go(-1)
+    props.actions.categoryCreate(values).then((res) => {
+      if (res && res.payload && res.payload.data && res.payload.data.status === 'success') {
+        props.history.go(-1)
+        message.success('Thêm thành công')
+      } else {
+        setRequired(res.payload.data.error)
+        setNotice(res.payload.data.message.name[0])
+      }
+    })
   }
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo)
   }
+
   return (
     <div>
       <h3>Category</h3>
       <Form layout="vertical" onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Form.Item
-          label="Tên danh mục:"
-          name="name"
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập danh mục',
-            },
-          ]}
-        >
+        <Form.Item label="Tên danh mục:" name="name" validateStatus={required === true ? 'error' : ''} help={notice}>
           <Input />
         </Form.Item>
         <Form.Item>
